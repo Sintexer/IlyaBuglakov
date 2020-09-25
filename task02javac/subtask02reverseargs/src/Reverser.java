f1d899afc37bae2405a48481940e28d085bfa646
Ilya Buglakov <neonlightnight@gmail.com>
2020-09-25 20:47:55 +0300

    added args format messsage

--- task02javac/subtask02reverseargs/src/Reverser.java
+++ task02javac/subtask02reverseargs/src/Reverser.java
@@ -1,7 +1,12 @@
-public class Reverser{
-	public static void main(String... args){
-		for (int i = args.length-1;i>=0;--i){
-			System.out.println(args[i]);
-		}
-	}
+public class Reverser {
+
+    private final static String ARGS_FROMAT = "Use pattern: [\"1st_arg\"] [\"2nd_arg\"] ... [\"Nth_arg\"]";
+
+    public static void main(String... args) {
+    	if(args.length==0)
+			System.out.println(ARGS_FROMAT);
+        for (int i = args.length - 1; i >= 0; --i) {
+            System.out.print(args[i] + " ");
+        }
+    }
 }
