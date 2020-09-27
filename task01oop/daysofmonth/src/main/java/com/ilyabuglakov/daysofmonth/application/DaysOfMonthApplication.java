ae5ed1fa4d7043f8daba3a3b77b1794f697ba44e
Ilya Buglakov <neonlightnight@gmail.com>
2020-09-27 13:13:26 +0300

    fixed project after the loss of .iml file

--- task01oop/daysofmonth/src/main/java/com/ilyabuglakov/daysofmonth/application/DaysOfMonthApplication.java
+++ task01oop/daysofmonth/src/main/java/com/ilyabuglakov/daysofmonth/application/DaysOfMonthApplication.java
@@ -2,6 +2,8 @@ package main.java.com.ilyabuglakov.daysofmonth.application;
 
 import main.java.com.ilyabuglakov.daysofmonth.service.MonthStatisticService;
 
+import java.time.DateTimeException;
+
 /**
  * <h1>DaysOfMonthApplication</h1>
  * DaysOfMonthApplication is a class, used to parse command line args
@@ -20,12 +22,11 @@ public class DaysOfMonthApplication {
      * This is the main method, that parses args and
      * finds the amount of days in a specific month,
      * with the help of MonthStatisticService.getDays(year, month) method
-     * @param args - if args[0] is "help", the user guide will be shown
-     * else - args[0] - year as int, args[1] - month as int.
+     * args[0] - year as int, args[1] - month as int.
      */
     public static void main(String[] args) {
         MonthStatisticService service = new MonthStatisticService();
-        if(args.length == 0 || args[0].equals("help")) {
+        if(args.length != 2) {
             printGuide();
             return;
         }
@@ -34,7 +35,7 @@ public class DaysOfMonthApplication {
             int year = Integer.parseInt(args[0]);
             int month = Integer.parseInt(args[1]);
             System.out.println(service.getDays(year, month));
-        }catch (NumberFormatException e){
+        }catch (DateTimeException | NumberFormatException e){
             System.err.println("Wrong arguments");
             printGuide();
         }
