4c7d07873c59b7a6e7c459a5fa3864275853bbad
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-19 21:28:46 +0300

    created BookRepository

--- task06book/src/main/java/com/ilyabuglakov/task06book/dal/repository/BookRepository.java
+++ task06book/src/main/java/com/ilyabuglakov/task06book/dal/repository/BookRepository.java
@@ -1,5 +1,37 @@
 package com.ilyabuglakov.task06book.dal.repository;
 
-public class BookRepository {
+import com.ilyabuglakov.task06book.dal.dao.BookSetDao;
+import com.ilyabuglakov.task06book.dal.specification.Specification;
+import com.ilyabuglakov.task06book.model.book.Book;
 
+import java.util.List;
+import java.util.Optional;
+
+public class BookRepository {
+    private static BookRepository instance = new BookRepository();
+    private BookSetDao dao;
+    
+    private BookRepository(){
+        dao = new BookSetDao();
+    }
+
+    public static BookRepository getInstance() {
+        return instance;
+    }
+
+    public void add(Book book){
+        dao.add(book);
+    }
+
+    public List<Book> getBooks(){
+        return dao.getAll();
+    }
+
+    public Optional<Book> getBookByCriteria(Specification<Book> criteria){
+        return dao.findByCriteria(criteria);
+    }
+
+    public List<Book> getBooksByCriteria(Specification<Book> criteria){
+        return dao.findAllByCriteria(criteria);
+    }
 }
