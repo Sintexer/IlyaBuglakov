86ea7caf3a32881d82171dc483fc3596a3b6d7bf
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-19 21:15:34 +0300

    changed findBy() method to return Optional<T>

--- task06book/src/main/java/com/ilyabuglakov/task06book/dal/dao/GenericDao.java
+++ task06book/src/main/java/com/ilyabuglakov/task06book/dal/dao/GenericDao.java
@@ -4,13 +4,14 @@ import com.ilyabuglakov.task06book.dal.specification.Specification;
 import com.ilyabuglakov.task06book.exception.DaoRemoveException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GenericDao<T> {
 
     void add(T newInstance);
     void remove(T existingObject) throws DaoRemoveException;
     List<T> getAll();
-    <S extends Specification<T>> T findByCriteria(S criteria);
+    <S extends Specification<T>> Optional<T> findByCriteria(S criteria);
     <S extends Specification<T>> List<T> findAllByCriteria(S criteria);
 
 }
