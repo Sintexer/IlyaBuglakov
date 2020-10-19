57cda715895ac0605a18f112a763558f586068c4
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-19 23:58:51 +0300

    added BookComparator

---
+++ task06book/src/main/java/com/ilyabuglakov/task06book/model/book/BookComparator.java
@@ -0,0 +1,39 @@
+package com.ilyabuglakov.task06book.model.book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    public static Comparator<Book> comparingName(){
+        return Comparator.comparing(Book::getName);
+    }
+
+    public static Comparator<Book> comparingYearOfPublishing(){
+        return Comparator.comparing(Book::getYearOfPublishing);
+    }
+
+    public static Comparator<Book> comparingNumberOfPages(){
+        return Comparator.comparing(Book::getNumberOfPages);
+    }
+
+    public static Comparator<Book> comparingPublishingHouse(){
+        return Comparator.comparing(Book::getPublishingHouse);
+    }
+
+    public static Comparator<Book> comparingAuthors(){
+        return Comparator.comparing(book -> book.getAuthors().stream()
+                .sorted()
+                .findFirst().orElse(""));
+    }
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return comparingName()
+                .thenComparing(comparingYearOfPublishing())
+                .thenComparing(comparingNumberOfPages())
+                .thenComparing(comparingPublishingHouse())
+                .thenComparing(comparingAuthors())
+                .compare(o1, o2);
+    }
+
+}
