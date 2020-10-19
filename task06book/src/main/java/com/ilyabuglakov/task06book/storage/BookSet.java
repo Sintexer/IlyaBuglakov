8c3c558249b162e1829c0dd69e7fb1421c14b227
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-19 21:15:02 +0300

    added removeBook() method

--- task06book/src/main/java/com/ilyabuglakov/task06book/storage/BookSet.java
+++ task06book/src/main/java/com/ilyabuglakov/task06book/storage/BookSet.java
@@ -2,10 +2,8 @@ package com.ilyabuglakov.task06book.storage;
 
 import com.ilyabuglakov.task06book.model.book.Book;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class BookSet {
@@ -19,16 +17,20 @@ public class BookSet {
         books = new HashSet<>();
     }
 
-    public void addBook(Book book){
+    public void addBook(Book book) {
         books.add(book);
+    }
+
+    public boolean removeBook(Book book){
+        return books.remove(book);
     }
 
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
 
-    public List<Book> getBooks(){
-        return new ArrayList<>(books);
+    public Set<Book> getBooks() {
+        return new HashSet<>(books);
     }
 
 }
