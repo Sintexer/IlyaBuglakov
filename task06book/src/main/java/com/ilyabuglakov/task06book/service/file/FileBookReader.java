6b86f107515502429c161286cd2442f1c8144224
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-20 21:26:45 +0300

    added Book io from/to file

---
+++ task06book/src/main/java/com/ilyabuglakov/task06book/service/file/FileBookReader.java
@@ -0,0 +1,42 @@
+package com.ilyabuglakov.task06book.service.file;
+
+import com.ilyabuglakov.task06book.model.book.Book;
+import com.ilyabuglakov.task06book.service.BookParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/**
+ * Pattern of File with Books:
+ * [Book.toString()]
+ * [Book.toString()]
+ * ...
+ * [Book.toString()]
+ * Each Book should be on its own line
+ * Book.toString() pattern:
+ * [name|numberOfPages|author,author,...,author|publishingHouse|yearOfPublishing]
+ */
+public class FileBookReader implements AutoCloseable {
+
+    Scanner reader;
+    BookParser parser = new BookParser();
+
+    public FileBookReader(String path) throws FileNotFoundException {
+        reader = new Scanner(new FileReader(new File(path)));
+    }
+
+    public boolean hasNext(){
+        return reader.hasNextLine();
+    }
+
+    public Book readBook(){
+        return parser.parse(reader.nextLine());
+    }
+
+    @Override
+    public void close() throws Exception {
+        reader.close();
+    }
+}
---
+++ task06book/src/main/java/com/ilyabuglakov/task06book/service/file/FileBookWriter.java
@@ -0,0 +1,25 @@
+package com.ilyabuglakov.task06book.service.file;
+
+import com.ilyabuglakov.task06book.model.book.Book;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileBookWriter implements AutoCloseable {
+
+    private FileWriter writer;
+
+    public FileBookWriter(String path) throws IOException {
+        writer = new FileWriter(path);
+    }
+
+    public void writeBook(Book book) throws IOException {
+        writer.write(book.toString());
+    }
+
+    @Override
+    public void close() throws Exception {
+        writer.close();
+    }
+
+}
