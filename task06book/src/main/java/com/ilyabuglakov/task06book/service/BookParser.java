c13639ab288baad9b3850b80ed6ea3e123b3a98e
Ilya Buglakov <neonlightnight@gmail.com>
2020-10-20 22:28:30 +0300

    added BookParseException

--- task06book/src/main/java/com/ilyabuglakov/task06book/service/BookParser.java
+++ task06book/src/main/java/com/ilyabuglakov/task06book/service/BookParser.java
@@ -1,5 +1,6 @@
 package com.ilyabuglakov.task06book.service;
 
+import com.ilyabuglakov.task06book.exception.BookParseException;
 import com.ilyabuglakov.task06book.model.book.Book;
 import com.ilyabuglakov.task06book.model.book.BookBuilder;
 
@@ -12,12 +13,19 @@ import java.util.List;
 /**
  * Example of String, containing book:
  * [voyna i mir|253|ilya,vasya bul, petya|Kamilfo|2020]
- * [name   |   pages |  authors   |  publishing house | year]
+ * pattern:
+ * [name|pages|authors|publishingHouse|year]
  */
 public class BookParser {
 
-    public Book parse(String bookString){
+    private final int MIN_BOOKSTRING_LENGTH = 6;
+
+    public Book parse(String bookString) throws BookParseException {
+        if(bookString.length()<MIN_BOOKSTRING_LENGTH){
+            throw new BookParseException("Wrong Book String pattern");
+        }
         BookBuilder bookBuilder = new BookBuilder();
+        bookString = bookString.substring(1, bookString.length()-1);
         List<String> fields = new ArrayList<>(Arrays.asList(bookString.split("\\|")));
         bookBuilder.setName(fields.get(0));
         bookBuilder.setNumberOfPages(Integer.parseInt(fields.get(1)));
