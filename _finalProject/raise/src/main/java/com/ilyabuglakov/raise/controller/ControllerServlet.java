e79019ef3ce7db381ad849342842fdad5c1d68e7
Илья Буглаков <Neonlightnight@gmail.com>
2020-12-17 03:24:18 +0300

    added controller and index servlets

---
+++ _finalProject/raise/src/main/java/com/ilyabuglakov/raise/controller/ControllerServlet.java
@@ -0,0 +1,20 @@
+package com.ilyabuglakov.raise.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+@WebServlet("/controller")
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/template/web.jsp").forward(req, resp);
+    }
+
+
+
+}
