60f382c31da1d9d96e9c64529496c5aa9aec3606
Илья Буглаков <Neonlightnight@gmail.com>
2020-12-21 14:15:52 +0300

    add: ServletContext as execute parameter

--- _finalProject/raise/src/main/java/com/ilyabuglakov/raise/model/command/Command.java
+++ _finalProject/raise/src/main/java/com/ilyabuglakov/raise/model/command/Command.java
@@ -1,9 +1,11 @@
 package com.ilyabuglakov.raise.model.command;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public interface Command {
-    void execute(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext);
+    void execute(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext) throws ServletException, IOException;
 }
--- _finalProject/raise/src/main/java/com/ilyabuglakov/raise/model/command/impl/ToIndexCommand.java
+++ _finalProject/raise/src/main/java/com/ilyabuglakov/raise/model/command/impl/ToIndexCommand.java
@@ -1,6 +1,7 @@
 package com.ilyabuglakov.raise.model.command.impl;
 
 import com.ilyabuglakov.raise.model.command.Command;
+import com.ilyabuglakov.raise.model.command.CommandName;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,13 +11,8 @@ import java.io.IOException;
 
 public class ToIndexCommand implements Command {
     @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext) {
-        try {
-            servletContext.getRequestDispatcher("/template/index.jsp").forward(request, response);
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void execute(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext)
+            throws ServletException, IOException {
+        servletContext.getRequestDispatcher(CommandName.linksProperties.getProperty("index")).forward(request, response);
     }
 }
