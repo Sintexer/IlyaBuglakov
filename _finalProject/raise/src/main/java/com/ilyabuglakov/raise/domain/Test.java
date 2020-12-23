2339655c5bb604f02b9e316ec5a69d8fb42bbecd
Илья Буглаков <Neonlightnight@gmail.com>
2020-12-24 00:45:21 +0300

    add: @Builder annotations

--- _finalProject/raise/src/main/java/com/ilyabuglakov/raise/domain/Test.java
+++ _finalProject/raise/src/main/java/com/ilyabuglakov/raise/domain/Test.java
@@ -1,11 +1,13 @@
 package com.ilyabuglakov.raise.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.Set;
 
 @Data
+@Builder
 @EqualsAndHashCode(callSuper = true)
 public class Test extends Entity {
     private String testName;
--- _finalProject/raise/src/main/java/com/ilyabuglakov/raise/domain/UserTestResult.java
+++ _finalProject/raise/src/main/java/com/ilyabuglakov/raise/domain/UserTestResult.java
@@ -1,9 +1,11 @@
 package com.ilyabuglakov.raise.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
+@Builder
 @EqualsAndHashCode(callSuper = false)
 public class UserTestResult extends Entity {
     private long userId;
